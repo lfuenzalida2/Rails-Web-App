f2541d62587ef22b0ec2d2333388d5260092cb71
lfuenzalida2 <lfuenzalida2@uc.cl>
2020-04-26 19:37:21 -0400

    hotfix: id vuelve a ser primary key, run queda como value

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2020_04_26_185011) do
     t.string "name"
   end
 
-  create_table "users", id: false, force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
