3918fb19a0573beddf4be0abd5711ac0499029a3
lfuenzalida2 <lfuenzalida2@uc.cl>
2020-04-26 12:31:36 -0400

    CRUD_User listo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {sessions: "users/sessions", registrations: "users/registrations"}
+  
+  #Read List
+  get "users", to: "users#index"
+
+  #Show Specific
+  get 'users/:id', to: 'users#show', as: 'user'
+
+  #Delete
+
   root "posts#index"
 end
---
+++ db/migrate/20200426034408_add_username_to_user.rb
@@ -0,0 +1,13 @@
+class AddUsernameToUser < ActiveRecord::Migration[5.2]
+  def change
+    # add_column :users, :name, :string
+    # add_column :users, :gender, :string
+    # add_column :users, :region, :string
+    # add_column :users, :birthday, :date
+    # add_column :users, :number,   :integer
+    # add_column :users, :description, :text
+    # add_column :users, :sexual_orientation, :string
+    # add_column :users, :interests, :string
+    # add_column :users, :photos, :string
+  end
+end
