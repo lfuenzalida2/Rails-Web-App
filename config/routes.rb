49112e03079769f34e2b154f110550770fa9d47e
Jprampad <jpramirez5@uc.cl>
2020-04-26 20:27:31 -0400

    CRUD local ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "posts#index"
-end
+  # CRUD's Routes
+  # CREATE
+    get 'locals/new', to: 'locals#new'
+    post 'locals', to: 'locals#create'
+  
+  # READ
+    get 'locals',  to: 'locals#index'
+    get 'locals/:id', to: 'locals#show', as: 'local'
+  
+  # UPDATE
+    get 'locals/:id/edit', to: 'locals#edit', as: :local_edit
+    patch 'locals/:id', to: 'locals#update'
+
+  # DELETE
+    delete 'locals/:id', to: 'locals#destroy', as: :local_delete
+  end
