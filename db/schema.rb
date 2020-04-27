2a3332e8137e514dbda73bf9f8842a93a35ea877
Jprampad <jpramirez5@uc.cl>
2020-04-26 20:38:13 -0400

    Reviews Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_223137) do
+ActiveRecord::Schema.define(version: 2020_04_27_003212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2020_04_26_223137) do
     t.string "direccion"
     t.string "tipo"
     t.integer "n_citas"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "id_local"
+    t.text "review"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
