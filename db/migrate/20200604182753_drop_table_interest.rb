fafb2088cb6d70bda978c056bb7c38a7d9e57554
lfuenzalida2 <42476356+lfuenzalida2@users.noreply.github.com>
2020-06-04 17:40:35 -0400

    db fixed (#16)

--- db/migrate/20200604182753_drop_table_interest.rb
+++
@@ -1,4 +0,0 @@
-class DropTableInterest < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,13 @@
 # frozen_string_literal: true
 
-Region.create(name: 'I Tarapaca')
-Region.create(name: 'II Antofagasta')
-Region.create(name: 'III Atacama')
-Region.create(name: 'IV Coquimbo')
-Region.create(name: 'V Valparaiso')
-Region.create(name: 'VI Libertador General Bernardo O\'Higgins')
-Region.create(name: 'VII Maule')
-Region.create(name: 'VIII Biobio')
-Region.create(name: 'IX Araucania')
-Region.create(name: 'X Los Lagos')
-Region.create(name: 'XI Aysén del General Carlos Ibañez del Campo')
-Region.create(name: 'XII Magallanes y Antártica Chilena')
-Region.create(name: 'Region Metropolitana de Santiago')
-Region.create(name: 'XIV Los Rios')
-Region.create(name: 'XV Arica y Parinacota')
-Region.create(name: 'XVI Ñuble')
-
-Interest.create(name: 'Deporte')
-Interest.create(name: 'Musica')
-Interest.create(name: 'Danza')
-Interest.create(name: 'Cocina')
-
-Orientation.create(name: 'Hombres')
-Orientation.create(name: 'Mujeres')
-
-
-Gender.create(name: 'Hombre')
-Gender.create(name: 'Mujer')
-
-Admin.create(rut: 1, admin: true)
-
+User.create(email: "example@dccitas.cl",
+             password: "123456",
+             rut:"12312312-3",
+              name:"Pedro Piedra",
+               birthday:"02/05/1997",
+                description:"Un chico alegre",
+                 photos:"none",
+                  gender:"male",
+                   interests:"musica",
+                    sexual_orientation:"female",
+                     region: "I")
