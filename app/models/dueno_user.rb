e8d7ee70691f1c52f9b0fcfa301a7c8227d6582f
Jprampad <jpramirez5@uc.cl>
2020-06-04 22:36:21 -0400

    First Associations

--- app/models/dueno_user.rb
+++ app/models/dueno_user.rb
@@ -3,6 +3,7 @@
 class DuenoUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :locals
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
+    has_one :user, through :local
 end
